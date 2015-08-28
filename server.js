c6b7e8356563c76426e7cb33b201c620a045e9b5
grrrgo <grrrgo@gmail.com>
2015-08-28 01:02:20 -0700

    practise mode

--- server.js
+++ server.js
@@ -148,33 +148,41 @@ app.get('/quiz', function (req, res) {
 });
 
 app.post('/practise', function (req, res) {
-    var cat;
-    switch(req.body[0]){
-        case "GKModel":
-            cat = GKModel;
-            break;
-        case "SQMModel":
-            cat = SQMModel;
-            break;
-        case "EPModel":
-            cat = EPModel;
-            break;
-        case "PMModel":
-            cat = PMModel;
-            break;
-        case "MAModel":
-            cat = MAModel;
-            break;
-        case "SVVModel":
-            cat = SVVModel;
-            break;
-        case "SCMModel":
-            cat = SCMModel;
-            break;
+    var jobs = [];
+    console.log(req.body);
+    if (req.body.GK) {
+        jobs.push(getQuestionFromModel(GKModel,req.body.GK))
     }
-    async.series([getQuestionFromModel(cat,5)], function (err,result) {
-        res.send(result[0]);
+    if (req.body.EP) {
+        jobs.push(getQuestionFromModel(EPModel,req.body.EP))
+    }
+    if (req.body.MA) {
+        jobs.push(getQuestionFromModel(MAModel,req.body.MA))
+    }
+    if (req.body.PM) {
+        jobs.push(getQuestionFromModel(PMModel,req.body.PM))
+    }
+    if (req.body.SQM) {
+        jobs.push(getQuestionFromModel(SQMModel,req.body.SQM))
+    }
+    if (req.body.SCM) {
+        jobs.push(getQuestionFromModel(SCMModel,req.body.SCM))
+    }
+    if (req.body.SVV) {
+        jobs.push(getQuestionFromModel(SVVModel,req.body.SVV))
+        }
+    async.parallel(jobs, function (err,result) {
+        var returnVal=[];
+        result.forEach(function (value, index ,array) {
+            for (var obj in value){
+                returnVal.push(value[obj])
+            }
+            if (index == array.length - 1) {
+                res.send(returnVal)
+            }
+        })
     })
+
 });
 
 app.post('/saveRecord', function (req, res) {
--- views/js/app.js
+++ views/js/app.js
@@ -135,6 +135,8 @@ app.controller('aboutCtrl', function ($q, $scope, $rootScope, $http, $location) 
 });
 
 app.controller('examCtrl', function ($q, $scope, $rootScope, $http, $location, $routeParams, ObserverService, $interval) {
+	$rootScope.questions = [];
+	$rootScope.report = {};
 	$scope.index =Number($routeParams.id);
 	$scope.previous = function(){
 		$location.path('/exam/'+(Number($routeParams.id) - 1));
@@ -239,6 +241,164 @@ app.controller('examCtrl', function ($q, $scope, $rootScope, $http, $location, $
 	});
 });
 
+app.controller('practiseCtrl', function($scope, $routeParams, $http, $rootScope, $location, ObserverService) {
+	var questionDistribution = {}, totalQuestions= 0;
+	$scope.index =Number($routeParams.id);
+	$scope.previous = function(){
+		$location.path('/practise/'+(Number($routeParams.id) - 1));
+		if ($scope.choice){
+			$rootScope.questions[$scope.index].answer = $scope.choice;
+		}
+
+	};
+	$scope.next = function(){
+		$location.path('/practise/'+(Number($routeParams.id) + 1));
+		if ($scope.choice){
+			$rootScope.questions[$scope.index].answer = $scope.choice;
+		}
+	};
+	$scope.quit = function () {
+		$rootScope.questions = [];
+		$location.url('/home')
+	};
+	$scope.logout = function () {
+		$http.post('/logout',$rootScope.user).success(function () {
+			$location.url('/');
+			checkSession.check();
+			$rootScope.currentUser = undefined;
+		})
+	};
+	$scope.submit = function () {
+		$rootScope.submited = true;
+		var epwrong = 0, gkwrong = 0, mawrong = 0, pmwrong = 0, scmwrong = 0, sqmwrong = 0, svvwrong = 0;
+		var postData = {
+			"username":$rootScope.currentUser.username,
+			"mode": "quiz",
+			"time": new Date(),
+			"score": 0,
+			"category": null,
+			epScore: 0,
+			gkScore: 0,
+			maScore: 0,
+			pmScore: 0,
+			scmScore: 0,
+			sqmScore: 0,
+			svvScore: 0
+		};
+		$rootScope.questions.forEach(function (value, index, array) {
+			if (value.answer != value.correctChoice){
+				$rootScope.wrong ++;
+				$rootScope.report.wrong.push(value);
+				switch (value.category){
+					case 'ep':
+						epwrong ++;
+						break;
+					case 'gk':
+						gkwrong ++;
+						break;
+					case 'mam':
+						mawrong ++;
+						break;
+					case 'pm':
+						pmwrong ++;
+						break;
+					case 'scm':
+						scmwrong ++;
+						break;
+					case 'sqm':
+						sqmwrong ++;
+						break;
+					case 'SVV':
+						svvwrong ++;
+						break;
+				}
+
+			}
+
+			if (index == array.length - 1){
+				console.log(totalQuestions);
+				postData.score = Math.floor((1-($rootScope.wrong/$rootScope.questionDistribution.total))*100);
+				$rootScope.report.score = postData.score;
+				$rootScope.report.epScore = $rootScope.questionDistribution.data.EP?(1-(epwrong/$rootScope.questionDistribution.data.EP))*100:null;
+				$rootScope.report.gkScore = $rootScope.questionDistribution.data.GK?(1-(gkwrong/$rootScope.questionDistribution.data.GK))*100:null;
+				$rootScope.report.maScore = $rootScope.questionDistribution.data.MA?(1-(mawrong/$rootScope.questionDistribution.data.MA))*100:null;
+				$rootScope.report.pmScore = $rootScope.questionDistribution.data.PM?(1-(pmwrong/$rootScope.questionDistribution.data.PM))*100:null;
+				$rootScope.report.scmScore = $rootScope.questionDistribution.data.SCM?(1-(scmwrong/$rootScope.questionDistribution.data.SCM))*100:null;
+				$rootScope.report.sqmScore = $rootScope.questionDistribution.data.SQM?(1-(sqmwrong/$rootScope.questionDistribution.data.SQM))*100:null;
+				$rootScope.report.svvScore = $rootScope.questionDistribution.data.SVV?(1-(svvwrong/$rootScope.questionDistribution.data.SVV))*100:null;
+				postData.epScore = $rootScope.report.epScore;
+				postData.gkScore = $rootScope.report.gkScore;
+				postData.maScore = $rootScope.report.maScore;
+				postData.pmScore = $rootScope.report.pmScore;
+				postData.scmScore = $rootScope.report.scmScore;
+				postData.sqmScore = $rootScope.report.sqmScore;
+				postData.svvScore = $rootScope.report.svvScore;
+				/*$http.post('/saveRecord', postData).success(function () {
+					$location.url('/report');
+				});*/
+				$location.url('/report');
+			}
+		});
+	};
+
+});
+
+app.controller('practiseConfCtrl', function($scope, $http, $rootScope, $location, ObserverService) {
+	$rootScope.questions = [];
+	$rootScope.report = {};
+	$rootScope.report.wrong = [];
+	$rootScope.wrong = 0;
+	$rootScope.questionDistribution = {
+		total : 0
+	};
+	$scope.GKValue = 5;
+	$scope.EPValue = 5;
+	$scope.MAValue = 5;
+	$scope.PMValue = 5;
+	$scope.SCMValue = 5;
+	$scope.SQMValue = 5;
+	$scope.SVVValue = 5;
+
+	var postData = {};
+
+	$scope.submit = function () {
+		if ($scope.GK) {
+			postData.GK = $scope.GKValue;
+			$rootScope.questionDistribution.total += postData.GK
+		}
+		if ($scope.EP) {
+			postData.EP = $scope.EPValue;
+			$rootScope.questionDistribution.total += postData.EP
+		}
+		if ($scope.MA) {
+			postData.MA = $scope.MAValue;
+			$rootScope.questionDistribution.total += postData.MA
+		}
+		if ($scope.PM) {
+			postData.PM = $scope.PMValue;
+			$rootScope.questionDistribution.total += postData.PM
+		}
+		if ($scope.SCM) {
+			postData.SCM = $scope.SCMValue;
+			$rootScope.questionDistribution.total += postData.SCM
+		}
+		if ($scope.SQM) {
+			postData.SQM = $scope.SQMValue;
+			$rootScope.questionDistribution.total += postData.SQM
+		}
+		if ($scope.SVV) {
+			postData.SVV = $scope.SVVValue;
+			$rootScope.questionDistribution.total += postData.SVVValue
+		}
+
+		$rootScope.questionDistribution.data = postData;
+
+		$http.post('/practise', postData).success(function (response) {
+			$rootScope.questions = response;
+			$location.url('practise/0')
+		})
+	}
+});
 
 app.config(function ($routeProvider, $httpProvider, $locationProvider) {
 	var checkLoggedIn = function ($q, $timeout, $http, $location, $rootScope) {
@@ -299,16 +459,23 @@ app.config(function ($routeProvider, $httpProvider, $locationProvider) {
 				loggedin: checkLoggedIn
 			}
 		}).
-		when('/practice', {
-			templateUrl: 'partials/practice.html',
-			controller: 'practiceCtrl',
+		when('/report', {
+			templateUrl: 'partials/report.html',
+			//controller: 'reportCtrl',
 			resolve: {
 				loggedin: checkLoggedIn
 			}
 		}).
-		when('/report', {
-			templateUrl: 'partials/report.html',
-			//controller: 'reportCtrl',
+		when('/practise', {
+			templateUrl: 'partials/practiseConf.html',
+			controller: 'practiseConfCtrl',
+			resolve: {
+				loggedin: checkLoggedIn
+			}
+		}).
+		when('/practise/:id', {
+			templateUrl: 'partials/practise.html',
+			controller: 'practiseCtrl',
 			resolve: {
 				loggedin: checkLoggedIn
 			}
