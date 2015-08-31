f8fc9502371b172de8e31e09e12b8a5f3b7cb489
STY <yangchunhuash@hotmail.com>
2015-08-31 01:54:08 -0700

    final

--- views/js/app.js
+++ views/js/app.js
@@ -173,6 +173,68 @@ app.controller('aboutCtrl', function ($q, $scope, $rootScope, $http, $location) 
 			$rootScope.currentUser = undefined;
 		})
 	};
+
+	$scope.mouseover1 = function () {
+		var oDiv1=document.getElementById("harry");
+		startMove(oDiv1, {opacity: 100});
+	};
+	$scope.mouseover2 = function () {
+		var oDiv2=document.getElementById("jaime");
+		startMove(oDiv2, {opacity: 100});
+	};
+	$scope.mouseover3 = function () {
+		var oDiv3=document.getElementById("neeru");
+		startMove(oDiv3, {opacity: 100});
+	};
+	$scope.mouseover4 = function () {
+		var oDiv4=document.getElementById("nikitha");
+		startMove(oDiv4, {opacity: 100});
+	};
+
+	function getStyle(obj, name) {
+		if(obj.currentStyle) {
+			return obj.currentStyle[name];
+		}
+		else {
+			return getComputedStyle(obj, false)[name];
+		}
+	}
+
+	function startMove(obj, json, fnEnd) {
+		clearInterval(obj.timer);
+		obj.timer=setInterval(function() {
+			var bStop=true;
+
+			for(var attr in json) {
+				var cur=0;
+				if(attr=="opacity") {
+					cur=Math.round(parseFloat(getStyle(obj, attr))*100);
+				}
+				else {
+					cur=parseInt(getStyle(obj, attr));
+				}
+
+				var speed=(json[attr]-cur)/8;
+				speed=speed>0?Math.ceil(speed):Math.floor(speed);
+
+				if(cur!=json[attr])
+					bStop=false;
+
+				if(attr=="opacity") {
+					obj.style.filter="alpha(opacity:" + (cur+speed) + ")";
+					obj.style.opacity=(cur+speed)/100;
+				}
+				else {
+					obj.style[attr]=cur+speed+"px";
+				}
+			}
+
+			if(bStop) {
+				clearInterval(obj.timer);
+				if(fnEnd) fnEnd();
+			}
+		}, 30)
+	}
 });
 
 app.controller('examCtrl', function ($q, $scope, $rootScope, $http, $location, $routeParams, ObserverService,$timeout) {
@@ -189,7 +251,6 @@ app.controller('examCtrl', function ($q, $scope, $rootScope, $http, $location, $
 	};
 
 	$scope.mouseover = function () {
-
 		startMove(div,{left: 0}, function () {
 			startMove(div,{opacity: 100, height: 480});
 		});
@@ -224,7 +285,7 @@ app.controller('examCtrl', function ($q, $scope, $rootScope, $http, $location, $
 					cur=parseInt(getStyle(obj, attr));
 				}
 
-				var speed=(json[attr]-cur)/4;
+				var speed=(json[attr]-cur)/3;
 				speed=speed>0?Math.ceil(speed):Math.floor(speed);
 
 				if(cur!=json[attr])
